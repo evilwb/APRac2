f4f9fd6c85965abfd577b5e470e32951038cf603
Dinopony <dinopony.gg@gmail.com>
2025-02-19 20:57:10 +0100

    Fixed Joba cage matches freezing the game on start

--- Container.py
+++ Container.py
@@ -632,7 +632,7 @@ def patch_free_challenge_selection(patch: Rac2ProcedurePatch, addresses: IsoAddr
     address = addresses.JOBA_ARENA_MENU_FUNC
     patch.write_token(APTokenTypes.WRITE, address + 0xDC, NOP)  # Enable pressing right
     patch.write_token(APTokenTypes.WRITE, address + 0x1CC, NOP)  # Enable pressing left
-    patch.write_token(APTokenTypes.WRITE, address + 0x2F0, NOP)  # Enable starting a challenge without requirements
+    patch.write_token(APTokenTypes.WRITE, address + 0x2F0, NOP * 2)  # Enable starting a challenge without requirements
     patch.write_token(APTokenTypes.WRITE, addresses.JOBA_ARENA_DISPLAY_PREV_FUNC + 0x288, NOP)  # Display "previous"
     patch.write_token(APTokenTypes.WRITE, addresses.JOBA_ARENA_DISPLAY_NEXT_FUNC + 0x364, NOP)  # Display "next"
 
