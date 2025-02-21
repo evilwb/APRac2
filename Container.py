21ef7cf6ef41c7d3ae0a6fcc0fd50516423ddd23
Dinopony <dinopony.gg@gmail.com>
2025-02-22 00:12:01 +0100

    Fixed Joba cage matches freezing the game on start (#33)
    * Fixed Joba cage matches freezing the game on start
    
    * Applied same change to Maktar arena code, since it also had a BEQL instruction

--- Container.py
+++ Container.py
@@ -624,7 +624,7 @@ def patch_free_challenge_selection(patch: Rac2ProcedurePatch, addresses: IsoAddr
     address = addresses.MAKTAR_ARENA_MENU_FUNC
     patch.write_token(APTokenTypes.WRITE, address + 0xDC, NOP)  # Enable pressing right
     patch.write_token(APTokenTypes.WRITE, address + 0x204, NOP)  # Enable pressing left
-    patch.write_token(APTokenTypes.WRITE, address + 0x348, NOP)  # Enable starting a challenge without requirements
+    patch.write_token(APTokenTypes.WRITE, address + 0x348, NOP * 2)  # Enable starting a challenge without requirements
     patch.write_token(APTokenTypes.WRITE, addresses.MAKTAR_ARENA_DISPLAY_PREV_FUNC + 0x290, NOP)  # Display "previous"
     patch.write_token(APTokenTypes.WRITE, addresses.MAKTAR_ARENA_DISPLAY_NEXT_FUNC + 0x324, NOP)  # Display "next"
 
@@ -632,7 +632,7 @@ def patch_free_challenge_selection(patch: Rac2ProcedurePatch, addresses: IsoAddr
     address = addresses.JOBA_ARENA_MENU_FUNC
     patch.write_token(APTokenTypes.WRITE, address + 0xDC, NOP)  # Enable pressing right
     patch.write_token(APTokenTypes.WRITE, address + 0x1CC, NOP)  # Enable pressing left
-    patch.write_token(APTokenTypes.WRITE, address + 0x2F0, NOP)  # Enable starting a challenge without requirements
+    patch.write_token(APTokenTypes.WRITE, address + 0x2F0, NOP * 2)  # Enable starting a challenge without requirements
     patch.write_token(APTokenTypes.WRITE, addresses.JOBA_ARENA_DISPLAY_PREV_FUNC + 0x288, NOP)  # Display "previous"
     patch.write_token(APTokenTypes.WRITE, addresses.JOBA_ARENA_DISPLAY_NEXT_FUNC + 0x364, NOP)  # Display "next"
 
